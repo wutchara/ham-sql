723162392adc5e69a7396798a827b850ee5c954c
wutchara <sstmuham@gmail.com>
2022-10-14 23:49:15 +0700

    Add logic to generate chart

--- python-ui/test-chart.py
+++ python-ui/test-chart.py
@@ -1,27 +1,88 @@
 from tkinter import *
+import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
+
+
+def getDisplayColumns():
+    return ['avermrval_0', 'avermrval_1']
+
+
+def readFile(fileName):
+    file1 = open("./data/" + fileName, "r+")
+    # print("Output of Read function is ")
+    # print(file1.read())
+
+    rows = file1.read().split('\n')
+    print("Rows count: ", len(rows))
+    dataRows = rows[1:]
+    print("Data Rows count: ", len(dataRows))
+    headerRow = rows[0].split(',')
+    print("Header Rows", headerRow)
+
+    # Modify rows
+    rows = []
+    for i in range(len(dataRows)):
+        row = dataRows[i]
+        columns = row.split(',')
+        rows.append(columns)
+
+    print("Rows", len(rows), rows[0])
+    print("Header Index 'avermrval_0'", headerRow.index(
+        'avermrval_0'), rows[0][headerRow.index('avermrval_0')])
+
+    result = {}
+    headerLen = len(headerRow)
+    for i in range(headerLen):
+        headerName = headerRow[i]
+        column = []
+
+        for val in rows:
+            if len(val) == headerLen:
+                column.append(val[i])
+
+        result[headerName] = column
+        result['Index'] = range(1, len(column) + 1)  # TODO:
+
+    return result, headerRow
 
 # plot function is created for
 # plotting the graph in
 # tkinter window
 
 
+def convertToInt(strArr):
+    return [int(x) for x in strArr]
+
+
 def plot():
+    result, headers = readFile("8LGSRWNN-phd-0.csv")
+    displayColumns = getDisplayColumns()
 
     # the figure that will contain the plot
-    fig = Figure(figsize=(5, 5),
+    fig = Figure(figsize=(100, 100),
                  dpi=100)
 
-    # list of squares
-    y = [i**2 for i in range(101)]
-
     # adding the subplot
-    plot1 = fig.add_subplot(111)
+    plot1 = fig.add_subplot()
+
+    x = result['Index']
+    y = []
+
+    for columnName in displayColumns:
+        # Convert to number type
+        result[columnName] = convertToInt(result[columnName])
+        y = y + result[columnName]
+
+    y = np.array(y)
 
     # plotting the graph
-    plot1.plot(y)
+    for columnName in displayColumns:
+        plot1.plot(x, result[columnName], label=columnName)
+
+    plot1.legend()
+    plot1.set_yticks(np.arange(y.min(), y.max(), 10))
 
     # creating the Tkinter canvas
     # containing the Matplotlib figure
